6d681d254c4a25c003e088740e7e1e10bd24cce3
Silas Alves <sialsan@hotmail.com>
2021-02-01 15:09:15 -0300

    Add CDN cache

--- index.js
+++ index.js
@@ -7,7 +7,29 @@ const { getMeta } = require("./lib/getMeta");
 const addon = express();
 const path = require('path');
 
-const respond = function (res, data) {
+const getCacheHeaders = function (opts) {
+  opts = opts || {}
+
+  if (!Object.keys(opts).length)
+    return false
+
+  let cacheHeaders = {
+    cacheMaxAge: 'max-age',
+    staleRevalidate: 'stale-while-revalidate',
+    staleError: 'stale-if-error'
+  }
+
+  return Object.keys(cacheHeaders).map(prop => {
+    const value = opts[prop]
+    if (!value) return false
+    return cacheHeaders[prop] + '=' + value
+  }).filter(val => !!val).join(', ')
+}
+
+const respond = function (res, data, opts) {
+  const cacheControl = getCacheHeaders(opts)
+  if (cacheControl)
+    res.setHeader('Cache-Control', `${cacheControl}, public`)
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Content-Type', 'application/json');
@@ -25,14 +47,24 @@ addon.get("/:language?/configure", async function (req, res) {
 addon.get("/:language?/manifest.json", async function (req, res) {
   const language = req.params.language || DEFAULT_LANGUAGE
   const resp = await getManifest(language)
-  respond(res, resp);
+  const cacheOpts = {
+    cacheMaxAge: 7 * 24 * 60 * 60, // 7 days
+    staleRevalidate: 14 * 24 * 60 * 60, // 14 days
+    staleError: 30 * 24 * 60 * 60, // 30 days
+  }
+  respond(res, resp, cacheOpts)
 });
 
 addon.get("/:language?/catalog/:type/:id.json", async function (req, res) {
   const language = req.params.language || DEFAULT_LANGUAGE
   const type = req.params.type
   const resp = await getCatalog(type, language)
-	respond(res, resp);
+	const cacheOpts = {
+    cacheMaxAge: 2 * 24 * 60 * 60, // 2 days
+    staleRevalidate: 14 * 24 * 60 * 60, // 14 days
+    staleError: 30 * 24 * 60 * 60, // 30 days
+  }
+  respond(res, resp, cacheOpts)
 });
 
 addon.get("/:language?/catalog/:type/:id/skip=:skip.json", async function (req, res) {
@@ -40,7 +72,12 @@ addon.get("/:language?/catalog/:type/:id/skip=:skip.json", async function (req, 
   const type = req.params.type
   const page = req.params.skip / 20 + 1
   const resp = await getCatalog(type, language, page)
-	respond(res, resp);
+	const cacheOpts = {
+    cacheMaxAge: 1 * 24 * 60 * 60, // 1 days
+    staleRevalidate: 14 * 24 * 60 * 60, // 14 days
+    staleError: 30 * 24 * 60 * 60, // 30 days
+  }
+  respond(res, resp, cacheOpts)
 });
 
 addon.get("/:language?/catalog/:type/:id/search=:query.json", async function (req, res) {
@@ -48,7 +85,12 @@ addon.get("/:language?/catalog/:type/:id/search=:query.json", async function (re
   const type = req.params.type
   const query = req.params.query
   const resp = await getSearch(type, language, query)
-	respond(res, resp);
+	const cacheOpts = {
+    cacheMaxAge: 3 * 24 * 60 * 60, // 3 days
+    staleRevalidate: 14 * 24 * 60 * 60, // 14 days
+    staleError: 30 * 24 * 60 * 60, // 30 days
+  }
+  respond(res, resp, cacheOpts)
 });
 
 addon.get("/:language?/catalog/:type/:id/genre=:genre.json", async function (req, res) {
@@ -57,7 +99,12 @@ addon.get("/:language?/catalog/:type/:id/genre=:genre.json", async function (req
   const [genre, num] = req.params.genre.split("&")
   const page = (num === undefined) ? undefined : (num.replace(/([^\d])+/gim, '')) / 20 + 1
   const resp = await getGenres(type, language, genre, page)
-	respond(res, resp);
+	const cacheOpts = {
+    cacheMaxAge: 2 * 24 * 60 * 60, // 2 days
+    staleRevalidate: 14 * 24 * 60 * 60, // 14 days
+    staleError: 30 * 24 * 60 * 60, // 30 days
+  }
+  respond(res, resp, cacheOpts)
 });
 
 addon.get("/:language?/meta/:type/:id.json", async function (req, res) {
@@ -65,7 +112,19 @@ addon.get("/:language?/meta/:type/:id.json", async function (req, res) {
   const type = req.params.type
   const tmdbId = req.params.id.split(":")[1];
   const resp = await getMeta(type, language, tmdbId)
-	respond(res, resp);
+  const cacheOpts = {
+    staleRevalidate: 20 * 24 * 60 * 60, // 20 days
+    staleError: 30 * 24 * 60 * 60, // 30 days
+  }
+  if (type == 'movie') {
+    // cache movies for 14 days:
+    cacheOpts.cacheMaxAge = 14 * 24 * 60 * 60
+  } else if (type == 'series') {
+    const hasEnded = !!((resp.releaseInfo || '').length > 5)
+    // cache series that ended for 14 days, otherwise 1 day:
+    cacheOpts.cacheMaxAge = (hasEnded ? 14 : 1) * 24 * 60 * 60
+  }
+  respond(res, resp, cacheOpts)
 });
 
 module.exports = addon;
