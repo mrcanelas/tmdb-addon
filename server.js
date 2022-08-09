7ac74723ddaa9f9e666bb8c012f2362528697cd6
mrcanelas <sialsan@hotmail.com>
2022-08-09 18:02:00 -0300

    test OS hostname

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // require serverless version
 const addon = require('./index.js')
+var os = require("os");
+var hostname = os.hostname();
 
 // create local server
 addon.listen(process.env.PORT || 7000, function () {
     console.log('Addon active on port 7000.');
     console.log('http://127.0.0.1:7000/[language]/manifest.json');
-    console.log(__dirname.split('/'))
+    console.log(hostname)
 });
