ad8b8a4dbdcb2c9978c520bad9a0fc2e7f80cefb
mrcanelas <sialsan@hotmail.com>
2022-10-02 13:07:56 -0300

    add Bottleneck

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Bottleneck = require("bottleneck");
 const addon = express();
 const { getCatalog } = require("./lib/getCatalog");
 const { getSearch } = require("./lib/getSearch");
@@ -8,6 +9,12 @@ const { getTmdb } = require("./lib/getTmdb");
 const { cacheWrapMeta } = require("./lib/getCache");
 const { landingTemplate } = require("./lib/getTemplate");
 const { getTrending } = require("./lib/getTrending");
+
+const limiter = new Bottleneck({
+  maxConcurrent: process.env.LIMIT_MAX_CONCURRENT || 5,
+  highWater: process.env.LIMIT_QUEUE_SIZE || 50,
+  strategy: Bottleneck.strategy.OVERFLOW,
+});
 
 const getCacheHeaders = function (opts) {
   opts = opts || {};
@@ -47,7 +54,7 @@ addon.get("/:language?/configure", async function (req, res) {
   const language = req.params.language || DEFAULT_LANGUAGE;
   const manifest = await getManifest(language);
   const landingHTML = await landingTemplate(manifest);
-  res.setHeader("Content-Type", "text/html; charset=UTF-8")
+  res.setHeader("Content-Type", "text/html; charset=UTF-8");
   res.send(landingHTML);
 });
 
@@ -62,32 +69,45 @@ addon.get("/:language?/manifest.json", async function (req, res) {
   respond(res, manifest, cacheOpts);
 });
 
-addon.get("/:language?/catalog/:type/:id/:extra?.json", async function (req, res) {
-  const { language, type, id } = req.params;
-  const {genre, skip, search} = req.params.extra ? Object.fromEntries(new URLSearchParams(req.url.split('/').pop().slice(0, -5)).entries()) : {}
-  const page = Math.ceil(skip ? skip / 20 + 1 : undefined ) || 1;
-  const metas = search ? await getSearch(type, language, search) : id === "tmdb.trending" ? await getTrending(type, id, language, genre, page) : await getCatalog(type, id, language, genre, page) 
-  const cacheOpts = {
-    cacheMaxAge: 7 * 24 * 60 * 60, // 7 days
-    staleRevalidate: 14 * 24 * 60 * 60, // 14 days
-    staleError: 30 * 24 * 60 * 60, // 30 days
-  };
-  if(id === "tmdb.trending" && genre === "Day") {
-    cacheOpts.cacheMaxAge = 1 * 24 * 60 * 60; // 1 day
+addon.get(
+  "/:language?/catalog/:type/:id/:extra?.json",
+  async function (req, res) {
+    const { language, type, id } = req.params;
+    const { genre, skip, search } = req.params.extra
+      ? Object.fromEntries(
+          new URLSearchParams(req.url.split("/").pop().slice(0, -5)).entries()
+        )
+      : {};
+    const page = Math.ceil(skip ? skip / 20 + 1 : undefined) || 1;
+    const metas = search
+      ? await getSearch(type, language, search)
+      : id === "tmdb.trending"
+      ? await getTrending(type, id, language, genre, page)
+      : await getCatalog(type, id, language, genre, page);
+    const cacheOpts = {
+      cacheMaxAge: 7 * 24 * 60 * 60, // 7 days
+      staleRevalidate: 14 * 24 * 60 * 60, // 14 days
+      staleError: 30 * 24 * 60 * 60, // 30 days
+    };
+    if (id === "tmdb.trending" && genre === "Day") {
+      cacheOpts.cacheMaxAge = 1 * 24 * 60 * 60; // 1 day
+    }
+    respond(res, metas, cacheOpts);
   }
-  respond(res, metas, cacheOpts);
-});
+);
 
 addon.get("/:language?/meta/:type/:id.json", async function (req, res) {
   const type = req.params.type;
   const tmdbId = req.params.id.split(":")[1];
   const language = req.params.language || DEFAULT_LANGUAGE;
-  const imdbId = req.params.id.split(":")[0]
+  const imdbId = req.params.id.split(":")[0];
 
   if (req.params.id.includes("tmdb:")) {
-    const resp = await cacheWrapMeta(`${language}:${tmdbId}`, async () => {
-      return await getMeta(type, language, tmdbId)
-    })
+    const resp = await cacheWrapMeta(`${language}:${tmdbId}`, () =>
+      limiter.schedule(async () => {
+        return await getMeta(type, language, tmdbId);
+      })
+    );
     const cacheOpts = {
       staleRevalidate: 20 * 24 * 60 * 60, // 20 days
       staleError: 30 * 24 * 60 * 60, // 30 days
@@ -103,26 +123,28 @@ addon.get("/:language?/meta/:type/:id.json", async function (req, res) {
     respond(res, resp, cacheOpts);
   }
   if (req.params.id.includes("tt")) {
-    const tmdbId = await getTmdb(type, imdbId)
-    if(tmdbId) {
-    const resp = await cacheWrapMeta(`${language}:${tmdbId}`, async () => {
-      return await getMeta(type, language, tmdbId)
-    })
-    const cacheOpts = {
-      staleRevalidate: 20 * 24 * 60 * 60, // 20 days
-      staleError: 30 * 24 * 60 * 60, // 30 days
-    };
-    if (type == "movie") {
-      // cache movies for 14 days:
-      cacheOpts.cacheMaxAge = 14 * 24 * 60 * 60;
-    } else if (type == "series") {
-      const hasEnded = !!((resp.releaseInfo || "").length > 5);
-      // cache series that ended for 14 days, otherwise 1 day:
-      cacheOpts.cacheMaxAge = (hasEnded ? 14 : 1) * 24 * 60 * 60;
-    }
-    respond(res, resp, cacheOpts);
+    const tmdbId = await getTmdb(type, imdbId);
+    if (tmdbId) {
+      const resp = await cacheWrapMeta(`${language}:${tmdbId}`, () =>
+        limiter.schedule(async () => {
+          return await getMeta(type, language, tmdbId);
+        })
+      );
+      const cacheOpts = {
+        staleRevalidate: 20 * 24 * 60 * 60, // 20 days
+        staleError: 30 * 24 * 60 * 60, // 30 days
+      };
+      if (type == "movie") {
+        // cache movies for 14 days:
+        cacheOpts.cacheMaxAge = 14 * 24 * 60 * 60;
+      } else if (type == "series") {
+        const hasEnded = !!((resp.releaseInfo || "").length > 5);
+        // cache series that ended for 14 days, otherwise 1 day:
+        cacheOpts.cacheMaxAge = (hasEnded ? 14 : 1) * 24 * 60 * 60;
+      }
+      respond(res, resp, cacheOpts);
     } else {
-      respond(res, {meta: {}})
+      respond(res, { meta: {} });
     }
   }
 });
